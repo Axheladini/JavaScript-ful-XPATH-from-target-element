395415f697446cd5f4e0b9eaccf48a4a174944d9
Agon Xheladini <agonxheladini001@gmail.com>
2020-12-23 21:11:49 +0100

    added additional function to find the indes of the node within parent node

--- xpath_generator.js
+++ xpath_generator.js
@@ -1,30 +1,34 @@
 
 function ax_xpath_generator(e){
     
-     var node = e.target;
-	   /*get the index of last element*/
-	   var last_node_index = Array.prototype.slice.call(node.parentElement.children).indexOf(node);
-		
-		if(node.parentElement.children.length == 1){ 
-		      var path = "/"+node.localName; 
+        var node = e.target;
+
+        var temp_one = helper.rrenja_get_element_index(node);
+		var last_node_index = Array.prototype.indexOf.call(temp_one, node);
+        
+
+		if(temp_one.length == 1){ 
+			    
+			var path = "/"+node.localName; 
 		}
-		else if(node.parentElement.children.length > 1){ 
-		      last_node_index = last_node_index +1;
-		      var path = "/"+node.localName+"["+last_node_index+"]"; 
+		else if(temp_one.length > 1){ 
+
+			 last_node_index = last_node_index +1;
+			 var path = "/"+node.localName+"["+last_node_index+"]"; 
 		}
-		
-    /*Loop the dom from the current element*/
-		while(node != document.html)
+
+
+		while(node != document.html && node.parentNode !== null)
 		{
-			 
+		
 			 node = node.parentNode;
-			 
+
 			 /* When loop reaches the last element of the dom (body)*/
 			 if(node.localName == "body")
 			 {  
-				 var current = "/body";
-				 path = current+path;
-				 break;
+					var current = "/body";
+					path = current+path;
+					break;
 			 }
 			 
 
@@ -37,35 +41,26 @@ function ax_xpath_generator(e){
 			 }
 
 
-			 /* if the node has class attribute and has no id attribute or is not the last element (body) */ 
+			 /* if the node has class attribute and has no id attribute or is not the last element */ 
 			 if(node.id == "" && node.localName != "body")
 			 {  
+
+                    if(node.parentNode !== null)
+                    {
+                       
+                       var temp = helper.rrenja_get_element_index(node);
+					   var node_index = Array.prototype.indexOf.call(temp, node);
 					
-					var temp = [];
-					var parentChildrens = node.parentElement.children;
-					var childrenNr = node.parentElement.children.length;
-
-					for(var i = 0; i <= childrenNr; i++)
-					{
-							if(typeof parentChildrens[i] !== "undefined")
-							{
-								if(parentChildrens[i].tagName.toLowerCase() == node.localName){ temp.push(parentChildrens[i]);}
-							} 
-					}
-
-					var node_index = Array.prototype.indexOf.call(temp, node);
-					
-					if(temp.length == 1){ 
-
-						var current = "/"+node.localName; 
-					
-					}
-					else if(temp.length > 1){ 
-
-						node_index = node_index+1;
-						var current = "/"+node.localName+"["+node_index+"]";
-
-					}
+						if(temp.length == 1)
+						{ 
+							var current = "/"+node.localName; 
+						}
+						else if(temp.length > 1)
+						{ 
+							node_index = node_index+1;
+							var current = "/"+node.localName+"["+node_index+"]";
+						}
+                    }
 							
 			 }
 			
@@ -74,4 +69,23 @@ function ax_xpath_generator(e){
 		}
 		
 		return path;
+}
+
+function rrenja_get_element_index(node)
+{
+
+   var temp = []; 
+   var parentChildrens = node.parentElement.children;
+   var childrenNr = node.parentElement.children.length;
+
+   for(var i = 0; i <= childrenNr; i++)
+   {
+      if(typeof parentChildrens[i] !== "undefined")
+      {
+      	if(parentChildrens[i].tagName.toLowerCase() == node.localName){ temp.push(parentChildrens[i]);}
+      }   
+   }
+
+  return temp;
+
 }
